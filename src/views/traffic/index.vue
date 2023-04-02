9f5e7c2d34b1898c3405456ffb41137a5c350413
tranthuhang <tranhang5701@gmail.com>
2023-04-02 09:04:53 +0700

    logout, menu

---
+++ src/views/traffic/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'Traffic',
+  components: { adminDashboard, editorDashboard },
+  data() {
+    return {
+      currentRole: 'adminDashboard'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'roles'
+    ])
+  },
+  created() {
+    if (!this.roles.includes('admin')) {
+      this.currentRole = 'editorDashboard'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
